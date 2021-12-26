4d2f2bf43e60c8969dfd983fe3aec67d0687e292
hyongti <hyongtiii@gmail.com>
2021-12-26 23:19:48 +0900

    useMutation 훅을 사용하여 onClick 이벤트 시 mutation 보내도록 수정

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,23 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { gql, useMutation } from "@apollo/client";
+import { AUTH_TOKEN } from "../constants";
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,6 +28,28 @@ const Login = () => {
     name: "",
   });
 
+  const [login] = useMutation(LOGIN_MUTATION, {
+    variables: {
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ login }) => {
+      localStorage.setItem(AUTH_TOKEN, login.token);
+      navigate("/");
+    },
+  });
+
+  const [signup] = useMutation(SIGNUP_MUTATION, {
+    variables: {
+      name: formState.name,
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ signup }) => {
+      localStorage.setItem(AUTH_TOKEN, signup.token);
+      navigate("/");
+    },
+  });
   return (
     <div>
       <h4 className="mv3">{formState.login ? "Login" : "Sign Up"}</h4>
@@ -53,7 +93,24 @@ const Login = () => {
       <div className="flex mt3">
         <button
           className="pointer mr2 button"
-          onClick={() => console.log("onClick")}
+          onClick={
+            formState.login
+              ? () =>
+                  login({
+                    variables: {
+                      email: formState.email,
+                      password: formState.password,
+                    },
+                  })
+              : () =>
+                  signup({
+                    variables: {
+                      name: formState.name,
+                      email: formState.email,
+                      password: formState.password,
+                    },
+                  })
+          }
         >
           {formState.login ? "login" : "create account"}
         </button>
